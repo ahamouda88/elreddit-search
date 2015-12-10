d06ca04845990d1df09dac471083acdfe2ad95a0
ahamouda88 <ahmed.hamouda8888@gmail.com>
2015-12-10 16:31:43 -0500

    Commit readme file

---
+++ readme.md
@@ -0,0 +1,10 @@
+# El-Reddit Search
+
+###### A Restful Webservice for searching comments using Reddit API.
+
+## Technologies Used:
+- Java 1.8
+- Jackson API
+- JUnit 4.10
+
+ 
--- src/test/java/com/elreddit/test/URLRequestTest.java
+++ src/test/java/com/elreddit/test/URLRequestTest.java
@@ -8,7 +8,6 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.elreddit.constants.URLConstants;
 import com.elreddit.request.URLRequest;
 import com.elreddit.request.URLRequestImpl;
 
@@ -20,22 +19,13 @@ public class URLRequestTest {
 	public void initTest(){
 		search = new URLRequestImpl();
 	}
-	@Test
-	public void testGetHttpResponse() throws IOException{
-		String query = "test";
-		String urlString = getURLString(query);		
-		URL url = new URL(urlString);
-		String result = search.getHttpResponse(url);
-		
-		Assert.assertNotNull(result);		
-	}
-	
+
 	@Test
 	public void testRandomURL() throws IOException{
-		String urlString = "https://www.google.com/";
+		String urlString = "https://www.reddit.com/";
 		URL url = new URL(urlString);
 		String result = search.getHttpResponse(url);
-		System.out.println(result);
+
 		Assert.assertNotNull(result);
 		
 	}
@@ -43,14 +33,5 @@ public class URLRequestTest {
 	@Test(expected = NullPointerException.class)
 	public void testNullException() throws IOException{
 		search.getHttpResponse(null);
-	}
-	
-	private String getURLString(String para){
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append(URLConstants.REDDIT_JSONSEARCH_URL);
-		sb.append(URLConstants.REDDIT_SEARCH_QUERY);
-		sb.append(para);
-		return sb.toString();
 	}
 }
