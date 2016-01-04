8f798b02c8c2cf4fc3121856f3e1ce3fdc2904fe
ahamouda88 <ahmed.hamouda8888@gmail.com>
2016-01-04 09:13:47 -0500

    Add Simple Comments

--- src/main/java/com/elreddit/constants/RedditConstants.java
+++ src/main/java/com/elreddit/constants/RedditConstants.java
@@ -1,7 +1,14 @@
 package com.elreddit.constants;
 
+/**
+ *	Constants being used in the Application.
+ *
+ */
 public interface RedditConstants{
 	
+	/*
+	 * URL Constants.
+	 */
 	String JSONSEARCH_URL = "https://www.reddit.com/r/subreddits/search.json";	
 	String SEARCH_QUERY = "?q=";
 	String AUTHOR_ATTRIBUTE = "author";
