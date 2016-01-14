d9ff1de0fd8f908cfc8f2cb61188a2639197a090
ahamouda88 <ahmed.hamouda8888@gmail.com>
2016-01-14 17:49:42 -0500

    Adding Constants

--- src/main/java/com/elreddit/constants/RedditConstants.java
+++ src/main/java/com/elreddit/constants/RedditConstants.java
@@ -12,4 +12,7 @@ public interface RedditConstants{
 	String JSONSEARCH_URL = "https://www.reddit.com/r/subreddits/search.json";	
 	String SEARCH_QUERY = "?q=";
 	String AUTHOR_ATTRIBUTE = "author";
+	
+	String USER_AGENT_PROP_VALUE = "linux:com.elreddit.request:v1.0";
+	String USER_AGENT_PROP_NAME = "User-Agent";
 }
